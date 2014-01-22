d2717926d6c2d703263e3c45c5c9c78a0f0cb80b
Jason Wells <jwells@realpurple-lm.sanfrancisco.corp.yahoo.com>
2014-01-22 12:43:04 -0800

    initial commit

---
+++ WeTrade/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeTrade
+//
+//  Created by Jason Wells on 1/22/14.
+//  Copyright (c) 2014 Jason Wells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
