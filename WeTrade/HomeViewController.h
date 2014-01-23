776e3cf9565884ae3b04f78bccf9ca554c611eb7
Jason Wells <jwells@realpurple-lm.sanfrancisco.corp.yahoo.com>
2014-01-23 13:42:01 -0800

    added basic storyboard and views

---
+++ WeTrade/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  WeTrade
+//
+//  Created by Jason Wells on 1/23/14.
+//  Copyright (c) 2014 Jason Wells. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController  : UIViewController
+
+@end
---
+++ WeTrade/MainViewController.m
@@ -0,0 +1,13 @@
+//
+//  MainViewController.m
+//  WeTrade
+//
+//  Created by Jason Wells on 1/23/14.
+//  Copyright (c) 2014 Jason Wells. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@implementation MainViewController
+
+@end
